1ee1bcee15e0f427dd1b1af217fc4a6310409bf3
John Moon <johnmoonyy@gmail.com>
2014-04-02 23:47:08 -0400

    Create static pages for info controller

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,22 @@
+class InfoController < ApplicationController
+  def home
+  end
+
+  def ecommerce
+  end
+
+  def marketing
+  end
+
+  def sales
+  end
+
+  def accounting
+  end
+
+  def outsourcing
+  end
+
+  def web_development
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+SmallBizTutorial::Application.routes.draw do
+  root to: 'info#home'
+  get "home", to: 'info#home'
+  get "ecommerce", to: 'info#ecommerce'
+  get "marketing", to: 'info#marketing'
+  get "accounting", to: 'info#accounting'
+  get "outsourcing", to: 'info#outsourcing'
+  get "web_development", to: 'info#web_development'
+  get "organization", to: 'info#organization'
+  get "sales", to: 'info#sales'
+end
